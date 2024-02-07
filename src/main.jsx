ac7bfa007027b93f5880f1fa19d11538a8f3a68c
ilfagaro <ildafabianagaonarotela@gmail.com>
2024-02-07 15:18:17 +0100

    change home to lowercase

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './views/home/Home'
+import Home from './views/home/home'
 import Index from './views/index'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Lottery from './views/lottery/Lottery'
